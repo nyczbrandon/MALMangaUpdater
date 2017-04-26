26f02fd489a85e3d8a245c096965805ee46a4c3c
niksoc <nikhil_2811@hotmail.com>
2017-04-26 15:11:48 +0530

    added sync bookmarks with mal, refactored popup.js

--- res/js/popup.js
+++ res/js/popup.js
@@ -10,22 +10,22 @@ $(document).ready(function() {
 
     //AJAX request to change the status of a manga in the user's list
     function mal_change_manga_status(mal_basicauth, manga_id, new_status) {
-        alert( 'manga id: ' + manga_id + ' ' + new_status ); 
-        return true;// request was successful (status 200)? 
+        alert( 'manga id: ' + manga_id + ' ' + new_status );
+        return true;// request was successful (status 200)?
     }
 
-    function display_bookmarks(obj) {
+    function display_bookmarks(mal_username) {
         var bookmarks_table = $('<table/>').css("list-style-type", "none").appendTo($("#bookmarks"));
-        chrome.storage.local.get('mimi_bookmarks_' + obj.mal_username, function(items) {
+        chrome.storage.local.get('mimi_bookmarks_' + mal_username, function(items) {
             if (chrome.runtime.lastError) {
                 console.err(chrome.runtime.lastError);
                 return;
             }
-            if(!items || !items['mimi_bookmarks_' + obj.mal_username]) {
+            if(!items || !items['mimi_bookmarks_' + mal_username]) {
                 return;
             }
-            var bookmarks = sortByKey(items['mimi_bookmarks_' + obj.mal_username], 'time');
-            $.each(bookmarks, function() {
+            var bookmarks_sorted = sortByKey(items['mimi_bookmarks_' + mal_username], 'time');
+            $.each(bookmarks_sorted, function() {
                 var tr = $('<tr/>')
                     .appendTo(bookmarks_table);
                 var bookmark_cell = $('<td/>')
@@ -33,7 +33,7 @@ $(document).ready(function() {
                 var a = $('<a/>')
                     .attr('href', this.link)
                     .text(this.text)
-                    .appendTo(bookmark_cell) 
+                    .appendTo(bookmark_cell)
                     .click(()=> {
                         chrome.tabs.create({url: this.link});
                         return false;
@@ -41,15 +41,27 @@ $(document).ready(function() {
                 $.each(['drop', 'completed'], (_, status )=>{
                     var button_cell = $('<td/>')
                         .appendTo(tr);
-                    var button = $('<button/>') 
+                    var button = $('<button/>')
                         .text( status )
-                        .appendTo(button_cell) 
-                        .click(()=> { 
+                        .appendTo(button_cell)
+                        .click(()=> {
                             chrome.storage.local.get('mal_basicauth', ({mal_basicauth})=> {
                                 var success = mal_change_manga_status(mal_basicauth, this.id, status);
                                 if(success) {
+                                    //remove table row
                                     tr.remove();
+                                    //remove from local storage
+                                    var bookmarks =  $.grep(items['mimi_bookmarks_' + mal_username], (bookmark) => {
+                                        return bookmark.id !== this.id;
+                                    });
+                                    var prop_name = 'mimi_bookmarks_' + mal_username;
+                                    var update_dict = {};
+                                    update_dict[prop_name] = bookmarks;
+                                    chrome.storage.local.set(update_dict, function(){
+                                        if(chrome.runtime.lastError) console.error(chrome.runtime.lastError);
+                                    });
                                 }
+                                else alert('Cannot reach MyAnimelist');
                             });
                         });
                     });
@@ -58,16 +70,18 @@ $(document).ready(function() {
 
     }
 
-    chrome.storage.local.get('mal_username', function(obj) {
-        if (obj.mal_username)
-        {
-            $('body').css("width", "350px");
-            $("#loginForm").css("display", "none"); 
-            $("#msg").css("display", "block");
-            $("#msg").text("Logged in as " + obj.mal_username);
+    function logged_in(mal_username) {
+        $('body').css("width", "350px");
+        $("#loginForm").css("display", "none");
+        $("#msg").css("display", "block");
+        $("#msg").text("Logged in as " + mal_username);
+        $("#bookmarks").css("display", "block");
+        display_bookmarks(mal_username);
+    }
 
-            $("#bookmarks").css("display", "block");
-            display_bookmarks(obj);
+    chrome.storage.local.get('mal_username', function(obj) {
+        if (obj.mal_username) {
+            logged_in(obj.mal_username);
         }
     });
 
@@ -85,9 +99,7 @@ $(document).ready(function() {
         })
         .done(function(data) {
             chrome.storage.local.set({'mal_username': mal_username, "mal_basicauth": mal_basicauth}, function() {
-                $("#loginForm").css("display", "none");
-                $("#msg").css("display", "block");
-                $("#msg").text("Logged in as " + mal_username);
+                logged_in(mal_username);
             });
         })
         .fail(function(data) {
